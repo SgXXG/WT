55cd8d112af29001e2279a742e306384b093c779
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-04-05 11:32:19 +0300

    one more task

---
+++ Lab4/task4_metoda.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task 4</title>
+
+    <style>
+        table {
+            border: 0;
+            border-spacing: 0;
+            border-collapse: collapse;
+        }
+
+        table td, table th {
+            border: 1px solid #000;
+        }
+    </style>
+</head>
+<body>
+
+<form method="post">
+    Введите количество записей для извлечения:<br>
+    <input type="text" name="count" required><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
+
+<?php
+if(isset($_POST['count'])){
+    if(intval($_POST['count']) > 0){
+
+        $mysqli = new mysqli('localhost', 'root', 'stas23', 'information_schema');
+
+        if($mysqli->connect_error) {
+            die('Ошибка при подключении к БД: ' . $mysqli->connect_error);
+        }
+
+        $result = $mysqli->query('SELECT * FROM COLLATIONS ORDER BY RAND() LIMIT '.intval($_POST['count']));
+
+        if($result->num_rows == 0) {
+            echo 'Table is empty!';
+        }
+        else {
+            echo '<table>';
+
+            echo '<tr>';
+            $columns = $mysqli->query('SHOW columns FROM COLLATIONS;');
+            $columnNames = [];
+            foreach ($columns as $column) {
+                $columnName = $column['Field'];
+                $columnNames[] = $columnName;
+                echo "<th>$columnName</th>";
+            }
+            echo '</tr>';
+
+            foreach ($result as $row) {
+                echo '<tr>';
+
+                foreach ($columnNames as $columnName) {
+                    echo "<td>$row[$columnName]</td>";
+                }
+
+                echo '</tr>';
+            }
+
+            echo '</table>';
+        }
+        mysqli_close($mysqli);
+    } else {
+        echo "You need > 0 nums";
+    }
+}
